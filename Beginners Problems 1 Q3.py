4ab7d537a7b893e6e8f6848b1c28a19501407329
Annieeeee <147971245+anne02233@users.noreply.github.com>
2024-05-01 19:20:49 +1200

    Create Beginners Problems 1 Q3.py

---
+++ Beginners Problems 1 Q3.py
@@ -0,0 +1,5 @@
+age_years = int(input("Enter your age in years: "))
+age_days = age_years * 365
+print(f"You are {age_days} days old.")
+days_to_100 = (100 - age_years) * 365
+print(f"In {days_to_100} days you will be 100 years old!")
